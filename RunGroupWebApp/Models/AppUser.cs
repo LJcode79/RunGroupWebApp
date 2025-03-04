80a0fc03b264cf4d21a8ad745bafb5483407a759
LJ <ljdb1999@gmail.com>
2025-03-04 14:05:46 -0800

    Set up Identity Framework

--- RunGroupWebApp/Models/AppUser.cs
+++ RunGroupWebApp/Models/AppUser.cs
@@ -1,19 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RunGroupWebApp.Models
 {
-    public class AppUser
+    public class AppUser : IdentityUser
     {
-        [Key]
-        public string Id { get; set; }
         public int? Pace { get; set; }
         public int? Mileage { get; set; }
         public string? ProfileImageUrl { get; set; }
         public string? City { get; set; }
         public string? State { get; set; }
         [ForeignKey("Address")]
-        public int? AddressId { get; set; }
+        public int? AddressId { get; set; } 
         public Address? Address { get; set; }
         public ICollection<Club> Clubs { get; set; }
         public ICollection<Race> Races { get; set; }
