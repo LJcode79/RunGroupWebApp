9d393b9b52cb5659ba75ef2964dc4bb05236c7dc
LJ <ljdb1999@gmail.com>
2025-03-06 14:48:39 -0800

    Completed User list and user detail page

---
+++ RunGroupWebApp/ViewModels/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroupWebApp.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public int? Mileage { get; set; }
+        public int? Pace { get; set; }
+    }
+}
