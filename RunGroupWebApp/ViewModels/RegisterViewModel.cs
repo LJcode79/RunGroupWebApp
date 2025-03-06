9e81a4da83a3ae0a9b2bb8aed0d5d128bdbd3902
LJ <ljdb1999@gmail.com>
2025-03-05 20:09:38 -0800

    Created Register page

--- RunGroupWebApp/ViewModels/RegisterViewModel.cs
+++ RunGroupWebApp/ViewModels/RegisterViewModel.cs
@@ -1,7 +1,19 @@
-﻿namespace RunGroupWebApp.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroupWebApp.ViewModels
 {
     public class RegisterViewModel
     {
-        public int MyProperty { get; set; }
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email address is required")]
+        public string EmailAddress { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is required")]
+        public string Password{ get; set; }
+        [Display(Name = "Confirm Password")]
+        [Required(ErrorMessage = "Confirm password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password do not match")]
+        public string ConfirmPassword { get; set; }
     }
 }
